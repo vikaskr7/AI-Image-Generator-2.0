88f123f751f65351daa041893350222b7bac8427
Vikas <vikaskrgth@gmail.com>
2023-04-19 19:57:15 +0530

    error fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://ai-image-generator-37ik.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
